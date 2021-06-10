4fef04d04dfee9ff47fd4a21a02508b3289bb544
joao <joao.camargo@gmail.com>
2021-06-09 22:00:59 -0300

    first commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by joao camargo on 09/06/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
